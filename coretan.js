0c0f573daf945cce824b1f5a590fba609e8bedb4
adiii257 <120394982+Adiii93@users.noreply.github.com>
2024-03-27 18:28:38 +0700

    Update coretan.js

--- coretan.js
+++ coretan.js
@@ -1,15 +1,1 @@
-const page1Button = document.getElementById("page1Button");
-const page2Button = document.getElementById("page2Button");
-const page3Button = document.getElementById("page3Button");
 
-page1Button.addEventListener("click", () => {
-  window.location.href = "isian.html";
-});
-
-page2Button.addEventListener("click", () => {
-  window.location.href = "page2.html";
-});
-
-page3Button.addEventListener("click", () => {
-  window.location.href = "page3.html";
-});
